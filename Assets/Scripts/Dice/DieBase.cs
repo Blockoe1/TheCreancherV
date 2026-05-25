c08aef62100a138e079e36f367b8917b539f190d
Lucas Fehlberg <lucasfehlberg06@gmail.com>
2026-05-25 13:45:07 -0500

    Dice Start

---
+++ Assets/Scripts/Dice/DieBase.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+/// <summary>
+/// The base class for all dice. Can also be used as the basic die variant
+/// </summary>
+public class DieBase : MonoBehaviour
+{
+    [SerializeField] private string _dieName = "Basic Die 1-6";
+    [SerializeField, Tooltip("DO NOT CHANGE THE NUMBER OF FACES. The effects of each face")] private DieFace[] dieFaces = new DieFace[6];
+
+    private int dieIndex = 0;
+
+    /// <summary>
+    /// The actual rolling of this die
+    /// </summary>
+    public void RollDie()
+    {
+        //Don't tell anyone that I'm not going to make the game break if there are more or less faces. Don't do it.
+        dieIndex = Random.Range(0, dieFaces.Length);
+    }
+
+    /// <summary>
+    /// Applying the effect of the die to the enemy's limb
+    /// </summary>
+    public void ApplyEffect(/*Target goes in here*/)
+    {
+        //target = target;
+        dieFaces[dieIndex].RollDie.Invoke();
+    }
+
+    /// <summary>
+    /// How much damage the die deals
+    /// </summary>
+    /// <param name="damage"></param>
+    public void FaceDamage(int damage)
+    {
+
+    }
+
+    /// <summary>
+    /// How much poison the die deals
+    /// </summary>
+    /// <param name="poison"></param>
+    public void FacePoison(int poison)
+    {
+
+    }
+
+    /// <summary>
+    /// How much healing the die deals
+    /// </summary>
+    /// <param name="healing"></param>
+    public void FaceSelfHeal(int healing)
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void FaceCorruption()
+    {
+
+    }
+}

a8d553a2faf7a321ff6283d5e26ae3eddadcfdc4
Lucas Fehlberg <lucasfehlberg06@gmail.com>
2026-05-26 11:21:42 -0500

    Spawn dice instances

--- Assets/Scripts/Managers/DiceManager.cs
+++ Assets/Scripts/Managers/DiceManager.cs
@@ -16,7 +16,7 @@ public class DiceManager : MonoBehaviour
     [SerializeField] private List<string> _discardBag;
     [SerializeField] private string _reservedDie;
 
-    private Dictionary<string, GameObject[]> diceLookup;
+    private Dictionary<string, GameObject[]> diceLookup = new();
 
     /// <summary>
     /// Initialize the dice bags
@@ -27,10 +27,37 @@ public class DiceManager : MonoBehaviour
         SetupDiceDict();
 
         //Initialize our dice bag
-
+        //TEMP
+        foreach (string die in allDiceDict.Keys)
+        {
+            for(int i = 0; i < Random.Range(1, 5); i++)
+            {
+                _drawBag.Add(die);
+            }
+        }
         //Grab the persistent data list of our dice and put it into the draw bag
         //Initialize the diceLookup - probably should gather a universal lookup table somewhere in a static. We can put that on the main menu
         //But for now, we'll keep that in here
+
+        //Setup the lookup table
+        foreach(string die in _drawBag)
+        {
+            if (!diceLookup.ContainsKey(die))
+            {
+                diceLookup.Add(die, new GameObject[3]);
+            }
+
+            //Forgive me for this
+            for (int i = 0; i < diceLookup[die].Length; i++)
+            {
+                if (diceLookup[die][i] == null)
+                {
+                    diceLookup[die][i] = Instantiate(allDiceDict[die], transform);
+                    diceLookup[die][i].SetActive(false);
+                    break;
+                }
+            }
+        }
     }
 
     /// <summary>

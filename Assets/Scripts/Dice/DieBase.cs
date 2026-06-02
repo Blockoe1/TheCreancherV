8669cf79e18c9d93db2e69f2cd129d4fc253b46b
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-06-02 11:21:40 -0500

    Looks like invert colors isnt working/

--- Assets/Scripts/Dice/DieBase.cs
+++ Assets/Scripts/Dice/DieBase.cs
@@ -31,7 +31,7 @@ public class DieBase : MonoBehaviour
     };
 
     [SerializeField] private string _dieName = "Basic Die 1-6";
-    [SerializeField] private string _dieDescription = "To be revealed in a future milestone...";
+    [SerializeField, TextArea] private string _dieDescription = "To be revealed in a future milestone...";
     [Header("Rolling Animation")]
     [SerializeField] private Vector3 localRotSpeed;
     [SerializeField] private Vector3 worldRotSpeed;

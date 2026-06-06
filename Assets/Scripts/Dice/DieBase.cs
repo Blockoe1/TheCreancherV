82508c7c330afd1042044b1ca152cacaae02d4ac
Lucas Fehlberg <lucasfehlberg06@gmail.com>
2026-06-06 15:26:41 -0500

    Stuff going on here

--- Assets/Scripts/Dice/DieBase.cs
+++ Assets/Scripts/Dice/DieBase.cs
@@ -142,14 +142,4 @@ public class DieBase : MonoBehaviour
             face.RefreshText();
         }
     }
-
-    private void OnMouseEnter()
-    {
-        Debug.Log("I am a mouse");
-    }
-
-    private void OnMouseExit()
-    {
-        Debug.Log("I am not a mouse");
-    }
 }

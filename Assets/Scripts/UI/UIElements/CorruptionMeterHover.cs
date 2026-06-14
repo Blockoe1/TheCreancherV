6677e82ea3a3ae1662f8d356668939adb0617029
WolfArcher12 <alexlindsey357@gmail.com>
2026-06-13 20:57:05 -0500

    Added Corruption Pop-up

---
+++ Assets/Scripts/UI/UIElements/CorruptionMeterHover.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CorruptionBarHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [TextArea]
+    [SerializeField]
+    private string description =
+        "Corruption increases the value of all dice by <b>2</b>.\n" +
+        "If more than half of your dice are corrupted, you die.";
+
+    [SerializeField] private CorruptionInfo popup;
+
+    private void Awake()
+    {
+        if (popup == null)
+        {
+            popup = GetComponentInParent<CorruptionInfo>();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (popup == null)
+        {
+            Debug.LogError("CorruptionBarHover: popup reference is null.");
+            return;
+        }
+
+        popup.Show("Corruption", description);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (popup == null)
+        {
+            Debug.LogError("CorruptionBarHover: popup reference is null.");
+            return;
+        }
+
+        popup.Hide();
+    }
+}
